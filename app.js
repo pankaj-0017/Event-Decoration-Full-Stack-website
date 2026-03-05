ac5e468ff9dd04fa0356aa383ea729fc4246f8fa
Pankaj Phalke <pankajphalke1717@gmail.com>
2026-03-05 22:18:04 +0530

    Add new feature

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const methodOverride = require("method-override");
 const Decoration = require("./models/decoration");
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -42,12 +45,22 @@ app.get("/decorations", async (req, res) => {
 
     let decorations = await Decoration.find({});
 
-    res.send(decorations);
+    res.render("decorations/index", { decorations });
 
 });
 
 app.get("/decorations/new", (req, res) => {
     res.render("decorations/new");
+});
+
+app.post("/decorations", async (req, res) => {
+
+    let newDecoration = new Decoration(req.body);
+
+    await newDecoration.save();
+
+    res.redirect("/decorations");
+
 });
 
 app.get("/decorations/:id", async (req, res) => {
@@ -56,11 +69,39 @@ app.get("/decorations/:id", async (req, res) => {
 
     let decoration = await Decoration.findById(id);
 
-    res.send(decoration);
+    res.render("decorations/show", { decoration });
 
 });
 
+app.get("/decorations/:id/edit", async (req, res) => {
 
+    let { id } = req.params;
+
+    let decoration = await Decoration.findById(id);
+
+    res.render("decorations/edit", { decoration });
+
+});
+
+app.put("/decorations/:id", async (req, res) => {
+
+    let { id } = req.params;
+
+    await Decoration.findByIdAndUpdate(id, req.body);
+
+    res.redirect(`/decorations/${id}`);
+
+});
+
+app.delete("/decorations/:id", async (req, res) => {
+
+    let { id } = req.params;
+
+    await Decoration.findByIdAndDelete(id);
+
+    res.redirect("/decorations");
+
+});
 /* Server */
 
 app.listen(3000, () => {
